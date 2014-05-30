a8e694a84f3e21195b4e1aa81e66520e70b7c39e
Clint Edwards <Clint.j.edwards@gmail.com>
2014-05-30 03:17:43 -0700

    Stage 1 of redesign

--- README.md
+++ README.md
@@ -4,10 +4,13 @@ JumpStart Alarm Clock
 Alarm clock application that will check time and play random music file from predefined directory upon set time.
 
   - Uses [Threading](https://docs.python.org/2/library/threading.html) module
-  - Uses [Time](https://docs.python.org/2/library/time.html) module
+  - Uses [Time](https://docs.python.org/2/library/time.html)
+  - Uses [Argparse](https://docs.python.org/dev/library/argparse.html) module
   - Uses/Requires [Afplay on Mac](https://developer.apple.com/library/mac/documentation/Darwin/Reference/Manpages/man1/afplay.1.html) binary
+  
   - Command line tool: Usage: Jumpstart.py hour minutes path_to_file|folder
-  - Usage example: python jumpstart.py 14 55 ~/Music/mymusic.mp3 &
+  - Usage example: python jumpstart.py 14 55 ~/Music/mymusic.mp3 & (Play a specific file)
+  - Usage example: python jumpstart.py 14 55 ~/Music/ & (Play a file from a folder at random)
   - **ONLY ACCEPTS MILITARY TIME** for now.
 
 > A small, quick project that I wanted to write for personal 
--- jumpstart.py
+++ jumpstart.py
@@ -5,7 +5,7 @@ Alarm clock script
 
 Checks time and plays random music from a predefined folder.
 
-Usage: jumpstart.py hour minutes full_path_to_folder|file &
+Usage: jumpstart.py UPDATE
 
 """
 
@@ -14,16 +14,39 @@ import os
 import time
 import sys
 import random
+import argparse
+import re
 from subprocess import call
 
 
 class jumpstart(threading.Thread):
+
 	
-	def __init__(self, hour, minute, music_source):
+	def __init__(self, time, music_source):
 		threading.Thread.__init__(self)
-		self.hour = hour
-		self.minute = minute
+		self.time = time
 		self.music_source = music_source
+
+	def convert_standard_time(self, time):
+		find_time = re.compile("^(1[0-2]|0?[1-9]):([0-5]?[0-9])")
+		
+		regex_result = find_time.search(time)
+		truncated_time = regex_result.group(0)
+
+		fully_transformed_time = truncated_time.replace(":", "")
+
+		find_time = re.compile("(am|pm)$")
+		regex_result = find_time.search(time)
+		daytime = regex_result.group(0)
+
+		if daytime == 'am':
+			military_time = fully_transformed_time
+			
+		elif daytime == 'pm':
+			military_time = int(fully_transformed_time) + 1200
+			
+		return military_time	
+	
 
 	#Check to see if its time to alarm every 30 seconds
 	def run(self):
@@ -84,12 +107,58 @@ class jumpstart(threading.Thread):
 
 
 def main():
+	
+	"""
+	add_argument explained:
+	required=False :: Make this an optional parameter.
+	action=store :: Store the value you get when this parameter is called.
+	nargs=1 :: Make sure there is at least 1 other argument after this flag is set.
+	metavar :: Change the help screen to better relect what should be after the flag.
+	"""
+	parser = argparse.ArgumentParser(description='JumpStart: The Command-Line AlarmClock')
+	parser.add_argument('-12','--standard', help="Use Standard Time in format: 4:23pm", required=False, action='store', nargs=1, metavar='TIME')
+	parser.add_argument('-24','--military', help="Use Military Time in format: 1523", required=False, action='store', nargs=1, metavar='TIME')
+	parser.add_argument('-r','--recent', help="View previous alarm settings", required=False, action='store_true')
+	parser.add_argument('-c','--clear_recent', help="Clear previous alarm settings", required=False, action='store_true')
+	parser.add_argument('-m','--music', help="Define music source(Path to file or folder)", required=False, action='store', nargs=1, metavar='PATH')
+	args = parser.parse_args()
+	#print args
+	#print len(sys.argv)
+	#print args.music
 
+	if len(sys.argv) == 1:
+		print "The current time is " + time.strftime("%Y-%m-%d %H:%M:%S")
+
+	#Standard time
+	if (args.standard 	  != None and
+	    args.military 	  == None and
+	    args.clear_recent == False and
+	    args.recent 	  == False):
+
+		regex = re.compile("^(1[0-2]|0?[1-9]):([0-5]?[0-9])(am|pm)$")
+		
+		if regex.match("".join(args.standard).lower()):
+			alarmclock = jumpstart("".join(args.standard).lower(), args.music)
+			alarmclock.convert_standard_time(alarmclock.time)
+
+	#Military time
+	if (args.standard 	  == None and
+	    args.military 	  != None and
+	    args.clear_recent == False and
+	    args.recent 	  == False):
+		pass
+
+		#alarmclock = jumpstart(time, music_source)
+		#print args.military
+
+"""
 	#Sanity checks
 	if len(sys.argv) == 1:
 		print "Usage: jumpstart.py hour minutes full_path_to_folder|file &"
 		print "\n"
 	else:
+
+		#Check to see that input is actually digits
 		if ((str(sys.argv[1]).isdigit()) and 
 			str(sys.argv[2]).isdigit()): 
 		
@@ -103,12 +172,20 @@ def main():
 			print "\n"
 			sys.exit()
 
+		#Check to see that input is between the right times
 		if (0 <= hours <= 23 and 
 			0 <= minutes <= 59):
 
-			alarmclock = jumpstart(hours,minutes,music_source)
+			#check to see if user wanted standard time
+			if sys.argv[3] == '-st':
+				alarmclock = jumpstart(hours,minutes,music_source)
+				alarmclock.use_standard_time = True
+				print alarmclock.use_standard_time
+			else:
+				alarmclock = jumpstart(hours,minutes,music_source)
+				print alarmclock.use_standard_time
 
-			print "Your Alarm has been set for " + str(hours) + ":" + str(minutes).zfill(2)
+			print "Your Alarm has been set for " + str(hours).zfill(2) + ":" + str(minutes).zfill(2)
 
 			alarmclock.start()
 		
@@ -118,6 +195,6 @@ def main():
 			print "Usage: jumpstart.py hour minutes full_path_to_folder|file &"	
 			print "Usage ex: jumpstart.py 14 55 ~/Music/mymusic.mp3"
 
-
+"""
 
 main() 
